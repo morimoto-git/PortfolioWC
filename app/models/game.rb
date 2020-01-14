9774b58ff836ec3d3f51c5dee08dc0e4bbd26e9f
Your Name <minamoyori19@outlook.jp>
2020-01-14 21:19:50 +0900

    game player/playing_time/level

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,4 +8,8 @@ class Game < ApplicationRecord
   def favorited_by?(user)
   	favorites.where(user_id: user.id).exists?
   end
+
+  enum player: ["~2人", "~4人", "~10人", "10人以上"]
+  enum playing_time: ["~5分", "~10分", "~20分", "20分以上"]
+  enum level: ["~3歳", "~7歳", "~12歳", "12歳以上"]
 end
