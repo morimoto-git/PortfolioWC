d594af4063066579caba462e88540f8d317044a4
Your Name <minamoyori19@outlook.jp>
2020-01-18 01:26:40 +0900

    modify fav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   def show
   	@user = User.find(params[:id])
     @games = @user.games.order(created_at: "DESC")
