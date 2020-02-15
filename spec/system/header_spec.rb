3dd6c4cd409443692ba200dd3b6d99af6f9b10c9
Your Name <minamoyori19@outlook.jp>
2020-02-15 22:35:12 +0900

    modify spec system/header

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -45,4 +45,54 @@ describe 'ヘッダーのテスト' do
       end
     end
   end
+
+  describe 'ログインしている場合' do
+    before do
+      @user = FactoryBot.create(:user)
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      click_button 'ログイン'
+    end
+    context 'ヘッダーの表示を確認' do
+      it 'サービスリンクが表示される' do
+        expect(page).to have_content 'サービス'
+      end
+      it 'ゲームリンクが表示される' do
+        expect(page).to have_content 'ゲーム'
+      end
+      it 'ランキングリンクが表示される' do
+        expect(page).to have_content 'ランキング'
+      end
+      it 'マイページリンクが表示される' do
+        expect(page).to have_content 'マイページ'
+      end
+      it 'ログアウトが表示される' do
+        expect(page).to have_content 'ログアウト'
+      end
+    end
+
+    context 'ヘッダーのリンクを確認' do
+      it 'アバウトページに遷移する' do
+        click_link 'サービス'
+        expect(current_path).to eq(about_path)
+      end
+      it 'ゲーム一覧ページに遷移する' do
+        click_link 'ゲーム'
+        expect(current_path).to eq(games_path)
+      end
+      it 'ランキングページに遷移する' do
+        click_link 'ランキング'
+        expect(current_path).to eq(ranking_path)
+      end
+      it 'マイページページに遷移する' do
+        click_link 'マイページ'
+        expect(current_path).to eq(user_path(@user))
+      end
+      it 'ログアウトする' do
+        click_link 'ログアウト'
+        expect(page).to have_content('ログイン')
+      end
+    end
+  end
 end
